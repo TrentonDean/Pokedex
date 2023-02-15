65ee549d6ad37aef76940028dcfd9d1ca695d815
Trenton Dean <trentondean@icloud.com>
2023-02-15 12:19:16 -0600

    added router and created pokemoninfo comp

---
+++ client/src/components/PokemonInfo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios"
+
+const PokemonInfo = () => {
+
+
+    return(
+        <div>
+            <p>This is the pokemon info page</p>
+        </div>
+    )
+}
+
+export default PokemonInfo
